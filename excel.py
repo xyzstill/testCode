8bd50038a846f2e92b1627c768242751a05a0d9f
xyzstill <54841867+xyzstill@users.noreply.github.com>
2020-01-25 02:30:32 +0800

    Update excel.py

--- excel.py
+++ excel.py
@@ -1,25 +1,36 @@
 from openpyxl import load_workbook
 from openpyxl import Workbook
+from openpyxl.utils import get_column_letter,column_index_from_string
+from openpyxl.styles import Font
+
+'''print(get_column_letter(2))
+print(column_index_from_string('D'))'''
 
 twb=Workbook()
 #twb=load_workbook('t.xlsx')
 
+
+
 tws=twb.active
-tws.title='tws'
-tws['A1']=42
+tws['A1']=2
 tws['E5']=42
 tws.append([1,2,3,4])
+tws.title='tws'
+print(type(tws['A1']))
+'''print(tuple(tws.rows))
+for row in tws.iter_cols(1,4,2,3):
+    for cell in row:
+        print(cell)
+print(tws.max_row)
+tws.row_dimensions[7].height=40'''
+tws.merge_cells('A1:E5')
+tws.unmerge_cells('A1:E5')
 
 tws1=twb.create_sheet('newSheet1')
 tws2=twb.create_sheet('newSheet2')
 tws3=twb.create_sheet('newSheet3')
 tws4=twb.create_sheet('newSheet4')
 
-tws5=twb['newSheet1']
+tws5=twb.copy_worksheet('Formula')
 
-'''for row in tws.iter_cols(1,4,2,3):
-    for cell in row:
-        print(cell)'''
-
-print(tuple(tws.rows))
 twb.save('t.xlsx')
